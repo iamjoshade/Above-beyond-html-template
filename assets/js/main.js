d7d56205a07839c6582383eef0af74cb3af1bc5d
iamjosh <58791428+iamjoshade@users.noreply.github.com>
2024-07-18 17:25:45 +0100

    Added dropdown style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,33 @@
-$(document).ready(function(){
-    $('.blog-post-carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        centerPadding: '50px',
+$(document).ready(function () {
+  $(".blog-post-carousel").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerPadding: "50px",
+    prevArrow: '<button class="PrevArrow d-lg-block d-none"></button>',
+    nextArrow: '<button class="NextArrow d-lg-block d-none"></button>',
 
-          // the magic
-  responsive: [{
-
-    breakpoint: 1024,
-    settings: {
-      slidesToShow: 2,
-      infinite: true
-    }
-
-  }, {
-
-    breakpoint: 600,
-    settings: {
-      slidesToShow: 1,
-      dots: false
-    }
-
-  }, {
-
-    breakpoint: 300,
-    settings: "unslick" // destroys slick
-
-  }]
-
-
-    });
+    // the magic
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 300,
+        settings: "unslick", // destroys slick
+      },
+    ],
   });
+});
