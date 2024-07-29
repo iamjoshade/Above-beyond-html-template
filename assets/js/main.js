19947dafd161d9c4a6901ceddafd21b18916aae0
iamjosh <58791428+iamjoshade@users.noreply.github.com>
2024-07-29 16:08:33 +0100

    added search functionality and styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,43 @@ $(document).ready(function () {
   // Search form jquery script
 
   
- 
+  var isOpen = false;
+  $(".searchbox-icon").click(function () {
+    if (isOpen == false) {
+      $(".searchbox").addClass("searchbox-open");
+      $(".searchbox-input").focus();
+      isOpen = true;
+    } else {
+      $(".searchbox").removeClass("searchbox-open");
+      $(".searchbox-input").focusout();
+      isOpen = false;
+    }
+  });
+  $(".searchbox-icon").mouseup(function () {
+    return false;
+  });
+  $(".searchbox").mouseup(function () {
+    return false;
+  });
+  $(document).mouseup(function () {
+    if (isOpen == true) {
+      $(".searchbox-icon").css("display", "block");
+      $(".searchbox-icon").click();
+    }
+  });
+  function buttonUp() {
+    var inputVal = $(".searchbox-input").val();
+    inputVal = $.trim(inputVal).length;
+    if (inputVal !== 0) {
+      $(".searchbox-icon").css("display", "none");
+    } else {
+      $(".searchbox-input").val("");
+      $(".searchbox-icon").css("display", "block");
+    }
+  }
+  $("input").keyup(function () {
+    buttonUp();
+  });
 
 
 });
